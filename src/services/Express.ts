2acdc8a76b6bddefec20bf786f0dc30fbd1e65b0
trygodson <gnqnpn@gmail.com>
2022-09-12 00:04:01 +0100

    something

--- src/services/Express.ts
+++ src/services/Express.ts
@@ -3,7 +3,14 @@ import cors from 'cors';
 import { MessageRoute, RoomRoute, UserRoute } from '../routes';
 
 export default async (app: Application) => {
-  app.use(cors({ origin: 'https://chat-fe.vercel.app' }));
+  app.use(
+    cors({
+      origin: 'https://chat-fe.vercel.app',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'X-Requested-With,content-type',
+      credentials: true,
+    }),
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
